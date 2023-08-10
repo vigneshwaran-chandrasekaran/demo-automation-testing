0e1dd777600d02e777f188031d7b82e3871a2e41
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2023-08-10 23:10:35 +0530

    antd and router packages are added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import type { DatePickerProps } from "antd";
+import { DatePicker, Space } from "antd";
 
 function App() {
+  const onChange: DatePickerProps["onChange"] = (date, dateString) => {
+    console.log(date, dateString);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Space direction="vertical">
+          <DatePicker onChange={onChange} />
+          <DatePicker onChange={onChange} picker="week" />
+          <DatePicker onChange={onChange} picker="month" />
+          <DatePicker onChange={onChange} picker="quarter" />
+          <DatePicker onChange={onChange} picker="year" />
+        </Space>
     </div>
   );
 }
