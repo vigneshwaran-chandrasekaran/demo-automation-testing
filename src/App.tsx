b7ca3444f4c8f3e3a4a273e66f3f40396214fea6
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2023-08-11 11:09:10 +0530

    formik and redux basic files are installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import type { DatePickerProps } from "antd";
 import { DatePicker, Space } from "antd";
+import { Counter } from "./components/Counter";
 
 function App() {
   const onChange: DatePickerProps["onChange"] = (date, dateString) => {
@@ -8,13 +9,14 @@ function App() {
 
   return (
     <div>
-        <Space direction="vertical">
-          <DatePicker onChange={onChange} />
-          <DatePicker onChange={onChange} picker="week" />
-          <DatePicker onChange={onChange} picker="month" />
-          <DatePicker onChange={onChange} picker="quarter" />
-          <DatePicker onChange={onChange} picker="year" />
-        </Space>
+      <Counter />
+      <Space direction="vertical">
+        <DatePicker onChange={onChange} />
+        <DatePicker onChange={onChange} picker="week" />
+        <DatePicker onChange={onChange} picker="month" />
+        <DatePicker onChange={onChange} picker="quarter" />
+        <DatePicker onChange={onChange} picker="year" />
+      </Space>
     </div>
   );
 }
