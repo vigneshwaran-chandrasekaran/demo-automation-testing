ff0ea83723c76673b7f7175f7d096ee63ef33095
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2023-11-02 21:30:07 +0400

    basic formik form added

--- src/components/Gold.form.tsx
+++ src/components/Gold.form.tsx
@@ -1,4 +1,5 @@
 import React, { FC, ReactNode } from "react";
+import { Formik, Field, Form, FormikHelpers } from "formik";
 
 // Define the component's prop types
 interface MyComponentProps {
@@ -7,13 +8,56 @@ interface MyComponentProps {
   children: ReactNode;
 }
 
+interface Values {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
 // Create a functional component
 const GoldForm: FC<MyComponentProps> = ({ prop1, prop2, children }) => {
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+  };
+
+  const handleOnSubmit = (
+    values: Values,
+    { setSubmitting }: FormikHelpers<Values>
+  ) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 500);
+  };
+
   return (
     <div>
       <h2>{prop1}</h2>
       <p>{prop2}</p>
       {children}
+      <div>
+        <h1>Signup</h1>
+        <Formik initialValues={initialValues} onSubmit={handleOnSubmit}>
+          <Form>
+            <label htmlFor="firstName">First Name</label>
+            <Field id="firstName" name="firstName" placeholder="John" />
+
+            <label htmlFor="lastName">Last Name</label>
+            <Field id="lastName" name="lastName" placeholder="Doe" />
+
+            <label htmlFor="email">Email</label>
+            <Field
+              id="email"
+              name="email"
+              placeholder="john@acme.com"
+              type="email"
+            />
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 };
