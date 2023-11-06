8dc280ef974cf7689e5233829d6a77ebf15cf2af
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2023-11-06 22:41:09 +0400

    gold form process started

--- src/components/Gold.form.tsx
+++ src/components/Gold.form.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { Formik, Field, Form, ErrorMessage, FormikHelpers } from "formik";
-import { Form as AntForm, Input, Button, Alert } from "antd";
+import { Form as AntForm, Input, Button } from "antd";
 import * as Yup from "yup";
 
 const { Item } = AntForm;
@@ -8,21 +8,35 @@ const { Item } = AntForm;
 interface IGoldFormProps {}
 
 interface FormValues {
-  name: string;
-  email: string;
+  aedGoldPrice: number;
+  inrGoldPrice: number;
+  aedToinr: number;
+  goldInGrams: number;
+  aedWastage: number;
+  aedVat: number;
+  aedCardCharges: number;
+  inrGst: number;
+  inrCardCharges: number;
 }
 
 const validationSchema = Yup.object().shape({
-  name: Yup.string().required("Name is required"),
-  email: Yup.string().email("Invalid email").required("Email is required"),
+  aedGoldPrice: Yup.number().required("AED Gold Price is required"),
+  inrGoldPrice: Yup.number().required("INR Gold price is required"),
 });
 
-const GoldForm: FC<IGoldFormProps> = () => {
-  const initialValues: FormValues = {
-    name: "",
-    email: "",
-  };
+const initialValues: FormValues = {
+  aedGoldPrice: 222.5,
+  inrGoldPrice: 5635,
+  aedToinr: 22.66,
+  goldInGrams: 20,
+  aedWastage: 10,
+  aedVat: 5,
+  aedCardCharges: 2.5,
+  inrGst: 3,
+  inrCardCharges: 0,
+};
 
+const GoldForm: FC<IGoldFormProps> = () => {
   const handleSubmit = (
     values: FormValues,
     { setSubmitting, resetForm }: FormikHelpers<FormValues>
@@ -43,14 +57,31 @@ const GoldForm: FC<IGoldFormProps> = () => {
     >
       {({ isSubmitting }) => (
         <Form>
-          <Item label="Name">
-            <Field type="text" name="name" as={Input} />
-            <ErrorMessage name="name" component={Alert} />
+          <Item
+            label="Gold Grams"
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+          >
+            <Field type="number" name="goldInGrams" as={Input} placeholder="Gold in Grams"  />
+            <ErrorMessage name="goldInGrams" />
           </Item>
 
-          <Item label="Email">
-            <Field type="text" name="email" as={Input} />
-            <ErrorMessage name="email" component={Alert} />
+          <Item
+            label="AED Gold Price"
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+          >
+            <Field type="number" name="aedGoldPrice" as={Input} />
+            <ErrorMessage name="aedGoldPrice" />
+          </Item>
+
+          <Item
+            label="INR Gold price"
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+          >
+            <Field type="number" name="inrGoldPrice" as={Input} />
+            <ErrorMessage name="inrGoldPrice" />
           </Item>
 
           <Item>
