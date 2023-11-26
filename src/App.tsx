38e0d84ffefbe235cbee2b6576a9479acd31cbf3
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2023-11-26 20:53:55 +0400

    breadcrumbs removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import React from "react";
-import { Breadcrumb, Layout } from "antd";
+import { Layout } from "antd";
 import { Header, Sidebar } from "./components/common";
 import { Router } from "./routes";
 
@@ -11,12 +11,7 @@ const App: React.FC = () => {
         <Header />
         <Layout>
           <Sidebar />
-          <Layout style={{ padding: "0 24px 24px" }}>
-            <Breadcrumb style={{ margin: "16px 0" }}>
-              <Breadcrumb.Item>Home vs</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>Main</Breadcrumb.Item>
-            </Breadcrumb>
+          <Layout style={{ padding: "24px 24px 24px" }}>
             <Router />
           </Layout>
         </Layout>
